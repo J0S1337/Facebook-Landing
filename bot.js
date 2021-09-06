c5cdd832c0bcad4f8e059e741a93df9099ec4e37
J0S1337 <75829929+J0S1337@users.noreply.github.com>
2021-09-06 15:22:06 +0200

    Delete bot.js

--- bot.js
+++
@@ -1,5 +0,0 @@
-const Discord = require("discord.js");
-const prefix = ['!']; 
-const client = new Discord.Client();
-client.login(process.env.TOKEN);
-client.user.setGame(setGame[i],`gtcrpg.ml | !help`);
